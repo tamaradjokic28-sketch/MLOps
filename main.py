3e8e0fcc57a8d3a57bea342ac8facb37577f8fdf
tamaradjokic28-sketch <tamaradjokic28@gmail.com>
2026-01-19 17:52:50 +0100

    Remove comments

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ app = FastAPI()
 
 @app.get("/score")
 def score_property():
-    #score = property.surface
-    #if property.rooms:
-    #    score += property.rooms * 10
     return return_score()
 
 
